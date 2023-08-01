9a431f55a120abc7de6e984074b63fc78ebd2601
Jia-Jia Zhu <dodge_waking_0b@icloud.com>
2023-08-01 09:51:00 -0700

    complete lab retry to pass test

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+// first set
+function saturdayFun(activity) {
+  if (activity === undefined) {
+    return "This Saturday, I want to roller-skate!";
+  } else {
+    return "This Saturday, I want to" + " " + activity + "!";
+  }
+}
+
+// second set
+function mondayWork(activity) {
+  if (activity === undefined) {
+    return "This Monday, I will go to the office.";
+  } else {
+    return "This Monday, I will" + " " + activity + ".";
+  }
+}
+
+//set 3
+function wrapAdjective(special) {
+  return function (description) {
+    return "You are " + special + description + special + "!";
+  };
+}
